c5171464f19f56d113943e557ed26e41d6c72eda
Karl Beck <k@b-coding.de>
2024-12-07 12:26:40 +0100

    images

--- startlocal.sh
+++ startlocal.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-/home/linuxbrew/.linuxbrew/bin/hugo server --disableFastRender --bind 0.0.0.0
+# Check the operating system
+OS=$(uname)
+
+if [ "$OS" == "Linux" ]; then
+    /home/linuxbrew/.linuxbrew/bin/hugo server --disableFastRender --bind 0.0.0.0
+elif [ "$OS" == "Darwin" ]; then
+    hugo server --disableFastRender --bind 0.0.0.0
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
